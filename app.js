8ca3c2380e12ea4e6c32413cf0f796e2a366b5c0
PazJal <30651575+PazJal@users.noreply.github.com>
2022-06-18 11:19:09 +0300

    feat: complete lecture 32

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const request = require('postman-request');
 
-const url = 'http://api.weatherstack.com/current?access_key=6b247cf1ef29b034fcd58e714fb05b0b&query=37.8267,-122.4233';
+const url = 'http://api.weatherstack.com/current?access_key=6b247cf1ef29b034fcd58e714fb05b0b&query=37.8267,-122.4233&units=s';
 
 request({url: url, json: true} , (error , response) => {
-  const {temperature , feelslike} = response.body.current;
-  console.log(`It is currently ${temperature}, it feels like ${feelslike}.`);
+  const {temperature , feelslike , weather_descriptions} = response.body.current;
+  console.log(`${weather_descriptions[0]}. It is currently ${temperature}, it feels like ${feelslike}.`);
 });
