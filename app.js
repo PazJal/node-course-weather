cb1a6074ae910b0e7c186b921d78484d9a794e75
PazJal <30651575+PazJal@users.noreply.github.com>
2022-06-18 20:42:43 +0300

    feat: complete lecture 34

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 const request = require('postman-request');
 const {weatherAPIKey , geoCodingAPIKey} = require('./localInfo');
 
-// const url = `http://api.weatherstack.com/current?access_key=${weatherAPIKey}&query=37.8267,-122.4233&units=s`;
+// const url = `http://api.weatherstack.com/current?access_key=${weatherAPIKey}&query=&37.8267,-122.4233units=s`;
 
 // request({url: url, json: true} , (error , response) => {
-//   const {temperature , feelslike , weather_descriptions} = response.body.current;
-//   console.log(`${weather_descriptions[0]}. It is currently ${temperature}, it feels like ${feelslike}.`);
+//   if(error) {
+//     console.log("Unable to connet to weather service.");
+//   } else if(response.body.error) {
+//     console.log("Unable to find location.");
+//   } else {
+//     const {temperature , feelslike , weather_descriptions} = response.body.current;
+//     console.log(`${weather_descriptions[0]}. It is currently ${temperature}, it feels like ${feelslike}.`);
+//   }
 // });
 
 const url = `https://eu1.locationiq.com/v1/search?key=${geoCodingAPIKey}&q=Los%20Angeles%20&format=json&limit=1`;
 
 request({url, json: true} , (error , response) => {
-  if(!error) {
+  if(error){
+    console.log("Unable to connect to Geolocation service.");
+  } else if (response.body.error) {
+    console.log("Geocoding failed.");
+  } else{
     const {lat, lon} = response.body[0];
     console.log(`lat: ${lat} lon: ${lon}`);
-  } else {
-    console.log(error);
   }
 });
