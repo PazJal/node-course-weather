cbf5fd006b63cf620c721552dd4bfcd956ad97e0
PazJal <30651575+PazJal@users.noreply.github.com>
2022-06-18 11:54:00 +0300

    feat: reset api keys

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const request = require('postman-request');
+const {weatherAPIKey , geoCodingAPIKey} = require('./localInfo');
 
-const url = 'http://api.weatherstack.com/current?access_key=6b247cf1ef29b034fcd58e714fb05b0b&query=37.8267,-122.4233&units=s';
+// const url = `http://api.weatherstack.com/current?access_key=${weatherAPIKey}&query=37.8267,-122.4233&units=s`;
 
-request({url: url, json: true} , (error , response) => {
-  const {temperature , feelslike , weather_descriptions} = response.body.current;
-  console.log(`${weather_descriptions[0]}. It is currently ${temperature}, it feels like ${feelslike}.`);
+// request({url: url, json: true} , (error , response) => {
+//   const {temperature , feelslike , weather_descriptions} = response.body.current;
+//   console.log(`${weather_descriptions[0]}. It is currently ${temperature}, it feels like ${feelslike}.`);
+// });
+
+const url = `https://eu1.locationiq.com/v1/search?key=${geoCodingAPIKey}&q=Los%20Angeles%20&format=json&limit=1`;
+
+request({url, json: true} , (error , response) => {
+  if(!error) {
+    const {lat, lon} = response.body[0];
+    console.log(`lat: ${lat} lon: ${lon}`);
+  } else {
+    console.log(error);
+  }
 });
